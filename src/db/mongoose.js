fccfe4b466b7d68c10216e082cc1ddbff140d997
HarshitaKalani <95532771+HarshitaKalani@users.noreply.github.com>
2022-10-09 10:41:59 +0530

    push

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,24 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const connectionUrl = process.env.MONGODB_URL;
+// const connectionUrl = process.env.MONGODB_URL;
 
 
-// const connectionURL
-mongoose.connect(connectionUrl, {
-    useNewUrlParser : true
-});
+// // const connectionURL
+// mongoose.connect(connectionUrl, {
+//     useNewUrlParser : true
+// });
+
+const {MongoClient} = require('mongodb')
+const url = 'mongodb://localhost:27017';
+const databasename ='taalloc';
+const client = new MongoClient(url);
+
+async function getData(){
+    let result= await client.connect();
+    db= result.db(databasename);
+    collection = db.collection('products');
+    let data =await collection.find({}).toArray();
+    console.log(data)
+
+}
+getData();
