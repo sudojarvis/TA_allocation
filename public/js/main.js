ddd3390b2e2ffb2fb75bdc07273cb98df2af8231
HarshitaKalani <95532771+HarshitaKalani@users.noreply.github.com>
2022-10-31 19:16:13 +0530

    push1123

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
+// {/* <><script language="JavaScript" type="text/javascript" src="/js/jquery-1.2.6.min.js"></script><script language="JavaScript" type="text/javascript" src="/js/jquery-ui-personalized-1.5.2.packed.js"></script><script language="JavaScript" type="text/javascript" src="/js/sprinkle.js"></script></> */}
+
+
 $(function() {
 	'use strict';
 	
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,16 @@
+//ALREADY EXISTED
+
 // const mongoose = require('mongoose');
 
 // const connectionUrl = process.env.MONGODB_URL;
 
 
-// // const connectionURL
+// const connectionURL
 // mongoose.connect(connectionUrl, {
 //     useNewUrlParser : true
 // });
+
+//ADDED BY US
 
 const {MongoClient} = require('mongodb')
 const url = 'mongodb://localhost:27017';
@@ -22,3 +26,14 @@ async function getData(){
 
 }
 getData();
+
+//CODE WITH HARRY
+// var mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost:27017/taalloc', {useNewUrlParser: true, useUnifiedTopology: true});
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//     // we're connected!
+//     console.log("Connected to database")
+//     });
+    
