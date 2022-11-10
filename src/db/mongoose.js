066646ed74b5ef7c3e43b54b9fc7509fe713cb90
HarshitaKalani <95532771+HarshitaKalani@users.noreply.github.com>
2022-11-11 01:03:59 +0530

    push

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,31 +1,30 @@
 //ALREADY EXISTED
 
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const connectionUrl = process.env.MONGODB_URL;
+const connectionUrl = process.env.MONGODB_URL;
 
 
-// const connectionURL
-// mongoose.connect(connectionUrl, {
-//     useNewUrlParser : true
-// });
+mongoose.connect(connectionUrl, {
+    useNewUrlParser : true
+});
 
 //ADDED BY US
 
-const {MongoClient} = require('mongodb')
-const url = 'mongodb://localhost:27017';
-const databasename ='taalloc';
-const client = new MongoClient(url);
+// const {MongoClient} = require('mongodb')
+// const url = 'mongodb://localhost:27017';
+// const databasename ='TA_alloc';
+// const client = new MongoClient(url);
 
-async function getData(){
-    let result= await client.connect();
-    db= result.db(databasename);
-    collection = db.collection('products');
-    let data =await collection.find({}).toArray();
-    console.log(data)
+// async function getData(){
+//     let result= await client.connect();
+//     db= result.db(databasename);
+//     collection = db.collection('products');
+//     let data =await collection.find({}).toArray();
+//     console.log(data)
 
-}
-getData();
+// }
+// getData();
 
 //CODE WITH HARRY
 // var mongoose = require('mongoose')
