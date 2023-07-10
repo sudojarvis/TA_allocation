a5cd123027cc2980270dd30b0347db63997d2e1e
rrhan32 <96813144+rrhan32@users.noreply.github.com>
2023-07-10 07:41:27 +0530

    files restored

--- public/js/deletecourse.js
+++ public/js/deletecourse.js
@@ -13,44 +13,41 @@ facultyForm.addEventListener("submit", function () {
         "password": password
     }
     console.log("entry got");
-    const filter = { courseCode: data.password }; // Replace with the filter for the entry you want to delete
-    const result =  db.PROFS.deleteOne(filter);
-    console.log(`${result.deletedCount} document(s) deleted`);
-});
-//     var url = '/checkpasswordprof';
-//     fetch(url, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(data)
-//     }).then((response) => {
-//         response.json().then((data) => {
-//             if (data.error) {
-//                 messageLoc.textContent = data.error;
-//             } else {
-//                 console.log(data.check);
-//                 if (data.check == "yes") {
 
-//                     var data = {
-//                         "courseCode": id
-//                     }
-//                     var url = '/deletecourse1';
-//                     fetch(url, {
-//                         method: 'POST',
-//                         headers: {
-//                             'Content-Type': 'application/json',
-//                         },
-//                         body: JSON.stringify(data)
-//                     }).then((response) => {
-//                         response.json().then((data) => {
-//                             if (data.error) {
-//                                 messageLoc.textContent = data.error;
-//                             }
-//                         });
-//                     });
-//                 }
-//             }
-//         });
-//     });
-// });
+    var url = '/checkpasswordprof';
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    }).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageLoc.textContent = data.error;
+            } else {
+                console.log(data.check);
+                if (data.check == "yes") {
+
+                    var data = {
+                        "courseCode": id
+                    }
+                    var url = '/deletecourse1';
+                    fetch(url, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(data)
+                    }).then((response) => {
+                        response.json().then((data) => {
+                            if (data.error) {
+                                messageLoc.textContent = data.error;
+                            }
+                        });
+                    });
+                }
+            }
+        });
+    });
+});
